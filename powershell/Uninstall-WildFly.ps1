2c8e8965ed3d8b2deef60f00ba3805ec324d6d2a
Florian Carrier <Akaizoku@users.noreply.github.com>
2020-01-24 12:35:17 +0000

    Clean-up installation path

--- powershell/Uninstall-WildFly.ps1
+++ powershell/Uninstall-WildFly.ps1
@@ -50,18 +50,17 @@ function Uninstall-WildFly {
     # --------------------------------------------------------------------------
     # Check environment variable
     if (Test-EnvironmentVariable -Variable $Properties.JBossHomeVariable -Scope $Properties.EnvironmentVariableScope) {
-      $JBossHomeVariable = Get-EnvironmentVariable -Variable $Properties.JBossHomeVariable -Scope $Properties.EnvironmentVariableScope
-      if (Test-Path -Path $JBossHomeVariable) {
-        if ($JBossHomeVariable -ne $Properties.JBossHomeDirectory) {
-          Write-Log -Type "WARN" -Object "The $($Properties.JBossHome) environment variable points to a different installation"
-          $Confirm = Confirm-Prompt -Prompt "Do you also want to remove $JBossHomeVariable?"
+      $JBossHome = Get-EnvironmentVariable -Variable $Properties.JBossHomeVariable -Scope $Properties.EnvironmentVariableScope
+      if (Test-Path -Path $JBossHome) {
+        if ($JBossHome -ne $Properties.JBossHomeDirectory) {
+          Write-Log -Type "WARN" -Object "The $($Properties.JBossHomeVariable) environment variable points to a different installation"
+          $Confirm = Confirm-Prompt -Prompt "Do you also want to remove $JBossHome?"
           if ($Confirm -Or ($Properties.UseEnvironmentVariable -And $Unattended)) {
             # Clean-up files
-            Write-Log -Type "INFO"  -Object "Removing files from $JBossHomeVariable"
-            Write-Log -Type "DEBUG" -Object $JBossHomeVariable
+            Write-Log -Type "INFO"  -Object "Removing files from $JBossHome"
             # TODO add catch if files are locked
             Start-Sleep -Seconds 1
-            Remove-Item -Path $JBossHomeVariable -Recurse -Force -Confirm:$Attended -ErrorVariable $RemoveErrors
+            Remove-Item -Path $JBossHome -Recurse -Force -Confirm:$Attended -ErrorAction "SilentlyContinue" -ErrorVariable $RemoveErrors
             # Check errors
             if ($RemoveErrors) {
               foreach ($RemoveError in $RemoveErrors) {
@@ -71,28 +70,28 @@ function Uninstall-WildFly {
               Write-Log -Type "WARN"  -Object "Please check and manually clear $JBossHome"
             }
             # Remove environment variable
-            Write-Log -Type "INFO" -Object "Removing $($Properties.JBossHome) environment variable"
+            Write-Log -Type "INFO" -Object "Removing $($Properties.JBossHomeVariable) environment variable"
             Remove-EnvironmentVariable -Variable $Properties.JBossHomeVariable -Scope $Properties.EnvironmentVariableScope -Confirm:$Attended
           }
         } else {
           # Remove environment variable
-          Write-Log -Type "INFO" -Object "Removing $($Properties.JBossHome) environment variable"
+          Write-Log -Type "INFO" -Object "Removing $($Properties.JBossHomeVariable) environment variable"
           Remove-EnvironmentVariable -Variable $Properties.JBossHomeVariable -Scope $Properties.EnvironmentVariableScope -Confirm:$Attended
         }
       } else {
-        Write-Log -Type "WARN" -Object "$($Properties.JBossHome) path not found $JBossHomeVariable"
+        Write-Log -Type "WARN" -Object "$($Properties.JBossHomeVariable) path not found $JBossHome"
         if ($Attended) {
           $Delete = Confirm-Prompt -Prompt "Do you want to remove the $Properties.JBossHomeVariable environment variable?"
         }
         if ($Unattended -Or $Delete) {
-          Write-Log -Type "INFO" -Object "Removing $($Properties.JBossHome) environment variable"
+          Write-Log -Type "INFO" -Object "Removing $($Properties.JBossHomeVariable) environment variable"
           Remove-EnvironmentVariable -Variable $Properties.JBossHomeVariable -Scope $Properties.EnvironmentVariableScope
         }
       }
     } else {
-      Write-Log -Type "INFO" -Object "$($Properties.JBossHome) environment variable is not defined"
+      Write-Log -Type "INFO" -Object "$($Properties.JBossHomeVariable) environment variable is not defined"
     }
-    # ------------------------------------------------------------------------
+    # --------------------------------------------------------------------------
     # Clean-up install location from configuration file
     if (Test-Path -Path $Properties.JBossHomeDirectory) {
       # Clean-up files
@@ -100,7 +99,7 @@ function Uninstall-WildFly {
       Write-Log -Type "DEBUG" -Object $Properties.JBossHomeDirectory
       # TODO add catch if files are locked
       Start-Sleep -Seconds 1
-      Remove-Item -Path $Properties.JBossHomeDirectory -Recurse -Force -Confirm:$Attended -ErrorVariable $RemoveErrors
+      Remove-Item -Path $Properties.JBossHomeDirectory -Recurse -Force -Confirm:$Attended -ErrorAction "SilentlyContinue" -ErrorVariable $RemoveErrors
       # Check errors
       if ($RemoveErrors) {
         foreach ($RemoveError in $RemoveErrors) {
@@ -110,6 +109,17 @@ function Uninstall-WildFly {
         Write-Log -Type "WARN"  -Object "Please check and manually clear $JBossHome"
       }
     }
+    # --------------------------------------------------------------------------
+    # Clean-up parent installation path
+    if (Test-Path -Path $Properties.InstallationPath) {
+      $RemainingContent = Get-ChildItem -Path $Properties.InstallationPath
+      if ($RemainingContent.Count -eq 0) {
+        Write-Log -Type "INFO"  -Object "Cleaning-up installation path"
+        Write-Log -Type "DEBUG" -Object $Properties.InstallationPath
+        Remove-Item -Path $Properties.InstallationPath -Recurse -Force -Confirm:$Attended
+      }
+    }
+    # --------------------------------------------------------------------------
     Write-Log -Type "CHECK" -Object "WildFly $($Properties.WildFlyVersion) uninstallation complete"
   }
 }
